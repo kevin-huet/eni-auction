acc04d73e304ca4cb6bd4fbde5833099c2caeca9
Artm95 <malleta01@gmail.com>
2021-04-09 12:55:53 +0200

    changement encheres

--- src/main/java/com/auction/eni_auction/dal/jdbc/EnchereJdbc.java
+++ src/main/java/com/auction/eni_auction/dal/jdbc/EnchereJdbc.java
@@ -17,44 +17,22 @@ import java.util.List;
 public class EnchereJdbc implements EnchereDAO {
 	private static final String SELECT_TOP = "SELECT TOP 1 montant_enchere, no_utilisateur FROM ENCHERES WHERE no_article = ? ORDER BY montant_enchere DESC";
 	private static final String INSERT = "INSERT INTO ENCHERES (no_utilisateur, no_article, date_enchere, montant_enchere) VALUES (?, ?, CURRENT_TIMESTAMP, ?)";
-	
+	private static final String SELECT_ONE = "SELECT no_utilisateur, no_article, date_enchere, montant_encheres FROM CATEGORIES WHERE no_utilisateur = ? AND no_article = ?";
+	private static final String UPDATE = "UPDATE ENCHERES SET date_enchere = CURRENT_TIMESTAMP, montant_encheres = ? WHERE no_utilisateur = ? AND no_article = ?";
 	@Override
     public Encheres insert(Encheres var) throws DALException, SQLException {
 		try (Connection cnx = ConnectionProvider.getConnection()) {
-			//get the previous top bid and refund its user's credit
-	        PreparedStatement pStmt = cnx.prepareStatement(SELECT_TOP);
-	        pStmt.setInt(1, var.getArticle().getNoArticle());
-	        ResultSet rs = pStmt.executeQuery();
-	        	
-	        while (rs.next()) {
-	        	int montant = rs.getInt(1);
-	        	int userId = rs.getInt(2);
-	        	StringBuilder statement = new StringBuilder();
-	        	statement.append("UPDATE UTILISATEURS SET credit = credit + ");
-	        	statement.append(montant);
-	        	statement.append(" WHERE no_utilisateur = ?");
-	        	PreparedStatement pStmt2 = cnx.prepareStatement(statement.toString());
-	        	pStmt2.setInt(1, var.getutilisateur().getNoUtilisateur());
-	        	pStmt2.executeQuery();
-	        }
+			this.updatePrevious(var.getNo_article(), cnx);
 	       
 	        //add the new bid and update its user's credit
         	PreparedStatement pStmt3 = cnx.prepareStatement(INSERT);
-        	pStmt3.setInt(1, var.getutilisateur().getNoUtilisateur());
+        	pStmt3.setInt(1, var.getUtilisateur().getNoUtilisateur());
         	pStmt3.setInt(2, var.getArticle().getNoArticle());
         	pStmt3.setInt(3, var.getMontantEnchere());
         
         	pStmt3.executeQuery();
         	
-        	StringBuilder statement2 = new StringBuilder();
-        	statement2.append("UPDATE UTILISATEURS SET credit = credit - ");
-        	statement2.append(var.getMontantEnchere());
-        	statement2.append(" WHERE no_utilisateur = ?");
-        	PreparedStatement pStmt4 = cnx.prepareStatement(statement2.toString());
-        	pStmt4.setInt(1, var.getutilisateur().getNoUtilisateur());
-        	pStmt4.executeQuery();
-        	var.getutilisateur().setCredit(var.getutilisateur().getCredit() - var.getMontantEnchere());
-        	var.getArticle().setEnchere(var);
+        	this.updateLatest(cnx, var);
 		} catch (SQLException e) {
         	DALException exception = new DALException();
 			exception.addError(e.getErrorCode());
@@ -65,38 +43,88 @@ public class EnchereJdbc implements EnchereDAO {
 		return var;
     }
 
-    @Override
-    public Encheres selectById(int id) throws DALException {
-        return null;
-    }
+	@Override
+	public Encheres selectById(int articleId, int utilisateurId) throws DALException {
+		Encheres enchere = null;
+    	try (Connection cnx = ConnectionProvider.getConnection()) {
+        	PreparedStatement pStmt = cnx.prepareStatement(SELECT_ONE);
+        	pStmt.setInt(1, utilisateurId);
+        	pStmt.setInt(2, articleId);
+        	ResultSet rs = pStmt.executeQuery();
+        	
+        	while (rs.next()) {
+        		enchere = new Encheres(
+        				rs.getInt("no_article"),
+        				rs.getInt("no_utilisateur"),
+        				rs.getInt("montant_enchere"),
+        				rs.getTimestamp("date_enchere").toLocalDateTime()
+        			);
+        	}
+        } catch (SQLException e) {
+        	DALException exception = new DALException();
+			exception.addError(e.getErrorCode());
+			throw exception;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return enchere;
+	}
 
-    @Override
-    public List<Encheres> selectAll() throws DALException {
-        return null;
-    }
+	@Override
+	public void update(Encheres var) throws DALException {
+		try (Connection cnx = ConnectionProvider.getConnection()) {
+			this.updatePrevious(var.getNo_article(), cnx);
+			
+        	PreparedStatement pStmt = cnx.prepareStatement(UPDATE);
+        	pStmt.setInt(1, var.getMontantEnchere());
+        	pStmt.setInt(2, var.getNo_utilisateur());
+        	pStmt.setInt(3, var.getNo_article());
+        	pStmt.executeQuery();
+        		
+        	this.updateLatest(cnx, var);
+        } catch (SQLException e) {
+        	DALException exception = new DALException();
+			exception.addError(e.getErrorCode());
+			throw exception;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
 
-    @Override
-    public void update(Encheres var) throws DALException {
+	@Override
+	public void delete(int articleId, int utilisateurId) throws DALException {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public void updatePrevious(int articleId, Connection cnx) throws SQLException {
+		//get the previous top bid and refund its user's credit
+        PreparedStatement pStmt = cnx.prepareStatement(SELECT_TOP);
+        pStmt.setInt(1, articleId);
+        ResultSet rs = pStmt.executeQuery();
+        	
+        while (rs.next()) {
+        	int montant = rs.getInt(1);
+        	int userId = rs.getInt(2);
+        	StringBuilder statement = new StringBuilder();
+        	statement.append("UPDATE UTILISATEURS SET credit = credit + ");
+        	statement.append(montant);
+        	statement.append(" WHERE no_utilisateur = ?");
+        	PreparedStatement pStmt2 = cnx.prepareStatement(statement.toString());
+        	pStmt2.setInt(1, userId);
+        	pStmt2.executeQuery();
+        }
+	}
+	
+	public void updateLatest(Connection cnx, Encheres var) throws SQLException {
+		StringBuilder statement2 = new StringBuilder();
+    	statement2.append("UPDATE UTILISATEURS SET credit = credit - ");
+    	statement2.append(var.getMontantEnchere());
+    	statement2.append(" WHERE no_utilisateur = ?");
+    	PreparedStatement pStmt4 = cnx.prepareStatement(statement2.toString());
+    	pStmt4.setInt(1, var.getNo_utilisateur());
+    	pStmt4.executeQuery();
+	}
 
-    }
-
-    @Override
-    public void delete(int id) throws DALException {
-
-    }
-
-    @Override
-    public List<Integer> getNoArticlesByUtilisateurAndEtat(Utilisateur utilisateur, String state) throws DALException, SQLException {
-        return null;
-    }
-
-    @Override
-    public List<Integer> getNoArticlesWonByUtilisateur(Utilisateur utilisateur) throws DALException, SQLException {
-        return null;
-    }
-
-    @Override
-    public HashMap<Integer, Integer> getAmountAndPseudoOfBestOffer(ArticlesVendus articleVendu) throws DALException, SQLException {
-        return null;
-    }
 }
